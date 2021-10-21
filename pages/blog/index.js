fa2362b4cec14d6cf75b20d41437ee49f5089266
HUZEFA SURATWALA <huzefa@pctoolmanconsulting.com>
2021-10-21 13:26:36 -0500

    adding posts

---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout'
+
+export default function BlogPage() {
+    return (
+        <Layout title="DevSpace Blog">
+            <h1>Blog</h1>
+        </Layout>
+    )
+}
